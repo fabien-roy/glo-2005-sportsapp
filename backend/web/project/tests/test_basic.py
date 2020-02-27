1df4625baccd0da79d76bd67d915510d0d262443
Fabien Roy <fabienroy28@gmail.com>
2020-02-27 14:26:21 -0500

    Add actual dummy test

---
+++ backend/web/project/tests/test_basic.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+
+from project import app, db
+
+TEST_DB = 'test.db'
+
+# TODO : Do actual tests
+# TODO : This test class can be used for login/register/logout
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(app.config['BASEDIR'], TEST_DB)
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+        self.assertEqual(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    def test_dummy(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
