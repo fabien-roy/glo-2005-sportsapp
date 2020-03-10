35cf37d20a906424bc5f4480a8c09969684d67ec
Fabien Roy <fabienroy28@gmail.com>
2020-03-09 23:54:24 -0400

    Format tests

--- app/tests/test_basic.py
+++ app/tests/test_basic.py
@@ -3,7 +3,8 @@ import unittest
 from app import app
 
 
-# TODO : This test class can be used for login/register/logout
+from app.tests.test_db_create import test_db_create
+
 
 class BasicTests(unittest.TestCase):
 
@@ -11,10 +12,13 @@ class BasicTests(unittest.TestCase):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
+        app.config['MYSQL_BD'] = 'test'
 
         self.app = app.test_client()
 
-        self.assertEqual(app.debug, False)
+        test_db_create()
+
+        self.assertEquals(app.debug, False)
 
     def tearDown(self):
         pass
--- app/tests/test_sports.py
+++ app/tests/test_sports.py
@@ -1,36 +1,20 @@
-import os
 import unittest
 
-from app import app, conn
 from app.models import Sport
 from app.queries import SportQuery
-from app.tests.test_db_create import test_db_create
+from app.tests import test_basic
 
 
-class SportsTests(unittest.TestCase):
+def add_sports():
+    sport1 = Sport(None, name='Randonnee')
+    sport2 = Sport(None, name='Escalade')
+    sport3 = Sport(None, name='Natation')
+    SportQuery().add(sport1)
+    SportQuery().add(sport2)
+    SportQuery().add(sport3)
 
-    def setUp(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = False
-        app.config['MYSQL_BD'] = 'test'
 
-        self.app = app.test_client()
-
-        test_db_create()
-
-        self.assertEquals(app.debug, False)
-
-    def tearDown(self):
-        pass
-
-    def add_sports(self):
-        sport1 = Sport(None, name='Randonnee')
-        sport2 = Sport(None, name='Escalade')
-        sport3 = Sport(None, name='Natation')
-        SportQuery().add(sport1)
-        SportQuery().add(sport2)
-        SportQuery().add(sport3)
+class SportsTests(test_basic.BasicTests):
 
     def test_sports_with_no_sport_should_display_no_sport(self):
         response = self.app.get('/sports/', follow_redirects=True)
@@ -42,7 +26,7 @@ class SportsTests(unittest.TestCase):
         self.assertNotIn(b'Natation', response.data)
 
     def test_sports_with_sports_should_display_sports(self):
-        self.add_sports()
+        add_sports()
         response = self.app.get('/sports/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'SportsApp', response.data)
@@ -52,7 +36,7 @@ class SportsTests(unittest.TestCase):
         self.assertIn(b'Natation', response.data)
 
     def test_sport_details_should_display_sport_details(self):
-        self.add_sports()
+        add_sports()
         response = self.app.get('/sports/1', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Randonnee', response.data)
