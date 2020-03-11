40150a63acb07b53b20d79b242cbd86d21591a0f
Fabien Roy <fabienroy28@gmail.com>
2020-03-11 09:24:01 -0400

    Add User table/model

--- app/users/models.py
+++ app/users/models.py
@@ -1,8 +1,11 @@
 class User:
-    def __init__(self, email, username, plaintext_password, first_name=None, last_name=None, telephone=None):
+    def __init__(self, username, email, plaintext_password, creation_date, last_login_date=None, first_name=None,
+                 last_name=None, phone_number=None):
         self.username = username
         self.email = email
         self.password = plaintext_password
-        self.firstName = first_name
-        self.lastName = last_name
-        self.telephone = telephone
+        self.creation_date = creation_date
+        self.last_login_date = last_login_date
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
