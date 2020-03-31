cf73bf93847d3f3b3d665ce78ac473894bbe07be
Fabien Roy <fabienroy28@gmail.com>
2020-03-30 20:22:46 -0400

    Add pratice center form style to templates

--- app/tests/forms.py
+++ app/tests/forms.py
@@ -11,21 +11,21 @@ class FakeForm:
         return True
 
     @staticmethod
-    def none_or_data(value):
-        return None if value is None else Data(value)
+    def empty_or_data(value):
+        return Data('') if value is None else Data(value)
 
 
 class FakeSportsForm(FakeForm):
 
     def __init__(self, name):
-        self.name = self.none_or_data(name)
+        self.name = self.empty_or_data(name)
 
 
 class FakePracticeCentersForm(FakeForm):
 
     def __init__(self, all=None, name=None, email=None, web_site=None, phone_number=None):
-        self.all = self.none_or_data(all)
-        self.name = self.none_or_data(name)
-        self.email = self.none_or_data(email)
-        self.web_site = self.none_or_data(web_site)
-        self.phone_number = self.none_or_data(phone_number)
+        self.all = self.empty_or_data(all)
+        self.name = self.empty_or_data(name)
+        self.email = self.empty_or_data(email)
+        self.web_site = self.empty_or_data(web_site)
+        self.phone_number = self.empty_or_data(phone_number)
