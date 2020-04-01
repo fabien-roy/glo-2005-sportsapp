251ddacb4273c183d84b599ade2fb862e832346d
Fabien Roy <fabienroy28@gmail.com>
2020-04-01 16:27:00 -0400

    Add tests for user recommendations

--- app/tests/test_recommendations_repositories.py
+++ app/tests/test_recommendations_repositories.py
@@ -1,7 +1,7 @@
 import unittest
 
 from app.repositories.mysql_recommendation_repositories import MySQLRecommendationsRepository
-from app.tests.fakes import sport1, sport2, sport3, center1, center2, center3
+from app.tests.fakes import sport1, sport2, sport3, center1, center2, center3, user3, user2, user1
 from app.tests.test_basic_repositories import BasicRepositoryTests
 
 
@@ -11,7 +11,7 @@ class RecommendationsRepositoryTests(BasicRepositoryTests):
         super().setUp()
         self.repository = MySQLRecommendationsRepository()
 
-    def test_get_all_for_sport_should_without_sport_get_no_recommendation(self):
+    def test_get_all_for_sport_without_sport_should_get_no_recommendation(self):
         self.reset_repositories()
         recommendations = self.repository.get_all_for_sport(sport1.id)
         self.assertEqual(0, len(recommendations))
@@ -28,7 +28,7 @@ class RecommendationsRepositoryTests(BasicRepositoryTests):
         recommendations = self.repository.get_all_for_sport(sport3.id)
         self.assertCountEqual(sport3.recommendations, recommendations)
 
-    def test_get_all_for_practice_center_should_without_practice_center_get_no_recommendation(self):
+    def test_get_all_for_practice_center_without_practice_center_should_get_no_recommendation(self):
         self.reset_repositories()
         recommendations = self.repository.get_all_for_practice_center(center1.id)
         self.assertEqual(0, len(recommendations))
@@ -44,6 +44,40 @@ class RecommendationsRepositoryTests(BasicRepositoryTests):
         self.assertCountEqual(center2.recommendations, recommendations)
         recommendations = self.repository.get_all_for_practice_center(center3.id)
         self.assertCountEqual(center3.recommendations, recommendations)
+
+    def test_get_all_for_sport_and_user_should_without_practice_center_get_no_recommendation(self):
+        self.reset_repositories()
+        recommendations = self.repository.get_all_for_sport_and_user(user1.username)
+        self.assertEqual(0, len(recommendations))
+        recommendations = self.repository.get_all_for_sport_and_user(user2.username)
+        self.assertEqual(0, len(recommendations))
+        recommendations = self.repository.get_all_for_sport_and_user(user3.username)
+        self.assertEqual(0, len(recommendations))
+
+    def test_get_all_for_sport_and_user_should_get_practice_center_recommendations_of_user(self):
+        recommendations = self.repository.get_all_for_sport_and_user(user1.username)
+        self.assertCountEqual(user1.sport_recommendations, recommendations)
+        recommendations = self.repository.get_all_for_sport_and_user(user2.username)
+        self.assertCountEqual(user2.sport_recommendations, recommendations)
+        recommendations = self.repository.get_all_for_sport_and_user(user3.username)
+        self.assertCountEqual(user3.sport_recommendations, recommendations)
+
+    def test_get_all_for_practice_center_and_user_should_without_practice_center_get_no_recommendation(self):
+        self.reset_repositories()
+        recommendations = self.repository.get_all_for_practice_center_and_user(user1.username)
+        self.assertEqual(0, len(recommendations))
+        recommendations = self.repository.get_all_for_practice_center_and_user(user2.username)
+        self.assertEqual(0, len(recommendations))
+        recommendations = self.repository.get_all_for_practice_center_and_user(user3.username)
+        self.assertEqual(0, len(recommendations))
+
+    def test_get_all_for_practice_center_and_user_should_get_practice_center_recommendations_of_user(self):
+        recommendations = self.repository.get_all_for_practice_center_and_user(user1.username)
+        self.assertCountEqual(user1.practice_center_recommendations, recommendations)
+        recommendations = self.repository.get_all_for_practice_center_and_user(user2.username)
+        self.assertCountEqual(user2.practice_center_recommendations, recommendations)
+        recommendations = self.repository.get_all_for_practice_center_and_user(user3.username)
+        self.assertCountEqual(user3.practice_center_recommendations, recommendations)
 
 
 if __name__ == "__main__":
