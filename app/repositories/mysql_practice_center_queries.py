e496ccf5fdb6e863b5f0938a15d1dc989ebbcbb6
Fabien Roy <fabienroy28@gmail.com>
2020-04-01 23:46:45 -0400

    Simplify practice center queries

--- app/repositories/mysql_practice_center_queries.py
+++ app/repositories/mysql_practice_center_queries.py
@@ -1,16 +1,18 @@
 from app.repositories.mysql_queries import MySQLQuery
 from app.repositories.mysql_tables import MySQLPracticeCentersTable
 
+all_fields_to_add = (MySQLPracticeCentersTable.name_col +
+                     ', ' + MySQLPracticeCentersTable.email_col +
+                     ', ' + MySQLPracticeCentersTable.web_site_col +
+                     ', ' + MySQLPracticeCentersTable.phone_number_col)
+
+all_fields = (MySQLPracticeCentersTable.id_col + ', ' + all_fields_to_add)
+
+select_all_operation = ('SELECT ' + all_fields + ' FROM ' + MySQLPracticeCentersTable.table_name)
+
 
 class MySQLPracticeCentersQuery(MySQLQuery):
     def get_all(self, form=None):
-        operation = ('SELECT ' + MySQLPracticeCentersTable.id_col +
-                     ', ' + MySQLPracticeCentersTable.name_col +
-                     ', ' + MySQLPracticeCentersTable.email_col +
-                     ', ' + MySQLPracticeCentersTable.web_site_col +
-                     ', ' + MySQLPracticeCentersTable.phone_number_col +
-                     ' FROM ' + MySQLPracticeCentersTable.table_name)
-
         inner_filtering = True
 
         if form is None:
@@ -39,26 +41,15 @@ class MySQLPracticeCentersQuery(MySQLQuery):
 
         orders = [MySQLPracticeCentersTable.name_col]
 
-        return self.build_query(operation, filters, orders, inner_filtering)
+        return self.build_query(select_all_operation, filters, orders, inner_filtering)
 
     def get(self, practice_center_id):
-        operation = ('SELECT ' + MySQLPracticeCentersTable.id_col +
-                     ', ' + MySQLPracticeCentersTable.name_col +
-                     ', ' + MySQLPracticeCentersTable.email_col +
-                     ', ' + MySQLPracticeCentersTable.web_site_col +
-                     ', ' + MySQLPracticeCentersTable.phone_number_col +
-                     ' FROM ' + MySQLPracticeCentersTable.table_name)
-
         filters = [self.filter_equal(MySQLPracticeCentersTable.id_col, practice_center_id)]
 
-        return self.build_query(operation, filters)
+        return self.build_query(select_all_operation, filters)
 
     def add(self):
         operation = ('INSERT INTO ' + MySQLPracticeCentersTable.table_name +
-                     ' (' + MySQLPracticeCentersTable.name_col +
-                     ', ' + MySQLPracticeCentersTable.email_col +
-                     ', ' + MySQLPracticeCentersTable.web_site_col +
-                     ', ' + MySQLPracticeCentersTable.phone_number_col + ')' +
-                     ' VALUES (%s, %s, %s, %s)')
+                     '(' + all_fields_to_add + ') VALUES (%s, %s, %s, %s)')
 
         return self.build_query(operation)
