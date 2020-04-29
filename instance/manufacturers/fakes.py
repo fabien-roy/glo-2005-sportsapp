a4da457a5176a720e1af23a094f9f6221ea1e6df
Fabien Roy <fabienroy28@gmail.com>
2020-04-28 20:36:27 -0400

    Add manufacturer module

--- instance/manufacturers/fakes.py
+++ instance/manufacturers/fakes.py
@@ -1,5 +1,5 @@
 from app.manufacturers.models import Manufacturer
 
-manufacturer1 = Manufacturer(None, name='Papa John')
-manufacturer2 = Manufacturer(None, name='Master Chief')
-manufacturer3 = Manufacturer(None, name='GLO Official')
+manufacturer1 = Manufacturer(1, name='Papa John')
+manufacturer2 = Manufacturer(2, name='Master Chief')
+manufacturer3 = Manufacturer(3, name='GLO Official')
