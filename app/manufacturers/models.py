3740f037e608f8b0da281a13217669df384d89bb
Fabien Roy <fabienroy28@gmail.com>
2020-04-28 20:46:48 -0400

    Fix code coverage

--- app/manufacturers/models.py
+++ app/manufacturers/models.py
@@ -2,8 +2,3 @@ class Manufacturer:
     def __init__(self, manufacturer_id, name):
         self.id = manufacturer_id
         self.name = name
-
-    def __eq__(self, other):
-        if isinstance(other, Manufacturer):
-            return self.id == other.id
-        return False
