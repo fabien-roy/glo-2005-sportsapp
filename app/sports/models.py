2cfb221b3a85865c189a7aa6634073e9274e73e5
Fabien Roy <fabienroy28@gmail.com>
2020-05-01 12:21:24 -0400

    Simplify fakes

--- app/sports/models.py
+++ app/sports/models.py
@@ -1,14 +1,14 @@
 class Sport:
-    def __init__(self, sport_id, name, climates=None, recommendations=None):
+    def __init__(self, sport_id, name, climates=None, recommendations=None,
+                 required_equipment_types=None):
         self.id = sport_id
         self.name = name
         self.climates = [] if climates is None else climates
         self.recommendations = [] if recommendations is None else recommendations
+        self.required_equipment_types = [] if required_equipment_types is None else \
+            required_equipment_types
 
     def __eq__(self, other):
         if isinstance(other, Sport):
             return self.id == other.id
         return False
-
-    def add_recommendation(self, recommendation):
-        self.recommendations.append(recommendation)
--- app/users/models.py
+++ app/users/models.py
@@ -2,12 +2,6 @@ class User:
     def __init__(self, username, email, first_name=None, last_name=None, phone_number=None,
                  creation_date=None, last_login_date=None, sport_recommendations=None,
                  practice_center_recommendations=None):
-        if sport_recommendations is None:
-            sport_recommendations = []
-
-        if practice_center_recommendations is None:
-            practice_center_recommendations = []
-
         self.username = username
         self.email = email
         self.first_name = first_name
@@ -15,16 +9,11 @@ class User:
         self.phone_number = phone_number
         self.creation_date = creation_date
         self.last_login_date = last_login_date
-        self.sport_recommendations = sport_recommendations
-        self.practice_center_recommendations = practice_center_recommendations
+        self.sport_recommendations = [] if sport_recommendations is None else sport_recommendations
+        self.practice_center_recommendations = [] if practice_center_recommendations is None else \
+            practice_center_recommendations
 
     def __eq__(self, other):
         if isinstance(other, User):
             return self.username == other.username
         return False
-
-    def add_sport_recommendation(self, sport_recommendation):
-        self.sport_recommendations.append(sport_recommendation)
-
-    def add_practice_center_recommendation(self, practice_center_recommendation):
-        self.practice_center_recommendations.append(practice_center_recommendation)
