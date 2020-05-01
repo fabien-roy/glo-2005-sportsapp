9a4f32787192dd3ade25b2d4b163ac123d710792
Fabien Roy <fabienroy28@gmail.com>
2020-05-01 16:41:09 -0400

    Fix weird names for fakes

--- instance/equipment_types/fakes.py
+++ instance/equipment_types/fakes.py
@@ -1,5 +1,5 @@
 from app.equipment_types.models import EquipmentType
 
-type1 = EquipmentType(1, name='hiking')
-type2 = EquipmentType(2, name='running')
-type3 = EquipmentType(3, name='recovery')
+type1 = EquipmentType(1, name='Hiking')
+type2 = EquipmentType(2, name='Running')
+type3 = EquipmentType(3, name='Recovery')
