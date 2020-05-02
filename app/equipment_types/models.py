24546c18ee43618a617d11420074348a445683f6
Fabien Roy <fabienroy28@gmail.com>
2020-05-01 22:09:39 -0400

    Fix equipment type repo test

--- app/equipment_types/models.py
+++ app/equipment_types/models.py
@@ -2,3 +2,8 @@ class EquipmentType:
     def __init__(self, type_id, name):
         self.id = type_id
         self.name = name
+
+    def __eq__(self, other):
+        if isinstance(other, EquipmentType):
+            return self.id == other.id
+        return False
--- tests/equipment_types/infrastructure/test_repositories.py
+++ tests/equipment_types/infrastructure/test_repositories.py
@@ -11,9 +11,9 @@ class EquipmentTypeRepositoryTests(RepositoryTests):
         self.repository = MySQLEquipmentTypeRepository(test_database)
 
     def test_get_all_for_sport_type_should_get_equipment_types_for_sport(self):
-        sports = self.repository.get_all_for_sport(sport1.id)
-        self.assertCountEqual(get_equipment_types_for_sport(sport1.id), sports)
-        sports = self.repository.get_all_for_sport(sport2.id)
-        self.assertCountEqual(get_equipment_types_for_sport(sport2.id), sports)
-        sports = self.repository.get_all_for_sport(sport3.id)
-        self.assertCountEqual(get_equipment_types_for_sport(sport3.id), sports)
+        types = self.repository.get_all_for_sport(sport1.id)
+        self.assertCountEqual(get_equipment_types_for_sport(sport1.id), types)
+        types = self.repository.get_all_for_sport(sport2.id)
+        self.assertCountEqual(get_equipment_types_for_sport(sport2.id), types)
+        types = self.repository.get_all_for_sport(sport3.id)
+        self.assertCountEqual(get_equipment_types_for_sport(sport3.id), types)
