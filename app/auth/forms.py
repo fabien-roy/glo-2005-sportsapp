a519ef890139b41245e24cb7477ecc784dab3ae3
Fabien Roy <fabienroy28@gmail.com>
2020-05-03 00:15:56 -0400

    Add styling to header

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,24 +4,35 @@ from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class RegisterForm(Form):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
-    confirm_password = PasswordField('Repeat Password', validators=[DataRequired(),
-                                                                    EqualTo('password')])
-    last_name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
-    first_name = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=50)])
-    telephone = StringField('Telephone number', validators=[DataRequired(), Length(min=10, max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)],
+                           render_kw={'placeholder': 'Username'})
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)],
+                             render_kw={'placeholder': 'Password'})
+    confirm_password = PasswordField('Repeat password', validators=[DataRequired(),
+                                                                    EqualTo('Password')],
+                                     render_kw={'placeholder': 'Repeat password'})
+    last_name = StringField('Last name', validators=[DataRequired(), Length(min=2, max=50)],
+                            render_kw={'placeholder': 'Last name'})
+    first_name = StringField('First name', validators=[DataRequired(), Length(min=2, max=50)],
+                             render_kw={'placeholder': 'First name'})
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=2, max=50)],
+                        render_kw={'placeholder': 'Email'})
+    telephone = StringField('Telephone number', validators=[DataRequired(), Length(min=10, max=20)],
+                            render_kw={'placeholder': 'Telephone number'})
     submit = SubmitField('Register')
 
 
 class LoginForm(Form):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)],
+                           render_kw={'placeholder': 'Username'})
+    password = PasswordField('Password', validators=[DataRequired()],
+                             render_kw={'placeholder': 'Password'})
     submit = SubmitField('Log in')
 
 
 class EditPasswordForm(Form):
-    new_password = PasswordField('New Password', validators=[])
-    new_confirm_password = PasswordField('Repeat New Password',
-                                         validators=[EqualTo('new_password')])
+    new_password = PasswordField('New password', validators=[],
+                                 render_kw={'placeholder': 'New password'})
+    new_confirm_password = PasswordField('Repeat new password',
+                                         validators=[EqualTo('new_password')],
+                                         render_kw={'placeholder': 'Repeat new password'})
