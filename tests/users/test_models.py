1e81e7e6e6c93e9572f330844fe73320bcd8f9e1
Fabien Roy <fabienroy28@gmail.com>
2020-05-03 22:51:06 -0400

    Add user model tests

--- tests/users/test_models.py
+++ tests/users/test_models.py
@@ -1,3 +1,4 @@
+from app.users.models import User
 from instance.users.fakes import user1, user2
 from tests.interfaces.test_basic import BasicTests
 
@@ -12,3 +13,12 @@ class UserTests(BasicTests):
 
     def test_equals_with_same_user_should_return_true(self):
         self.assertTrue(user1 == user1)
+
+    def test_is_authenticated_should_return_true(self):
+        self.assertTrue(User.is_authenticated())
+
+    def test_is_active_should_return_true(self):
+        self.assertTrue(User.is_active())
+
+    def test_is_anonymous_should_return_true(self):
+        self.assertFalse(User.is_anonymous())
