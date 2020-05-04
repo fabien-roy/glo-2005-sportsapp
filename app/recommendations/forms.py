a4a75da8525146a167aa5786bb1510d209cddf3f
Fabien Roy <fabienroy28@gmail.com>
2020-05-04 14:40:59 -0400

    Fix number of stars in modal

--- app/recommendations/forms.py
+++ app/recommendations/forms.py
@@ -11,7 +11,7 @@ def range_to_select_choices(items):
 
 
 class AddRecommendationForm(FlaskForm):
-    note = SelectField('Note', choices=range_to_select_choices(range(1, 5)), validate_choice=False)
+    note = SelectField('Note', choices=range_to_select_choices(range(1, 6)), validate_choice=False)
     comment = StringField('Comment', validators=[DataRequired(), Length(min=0, max=1000)],
                           render_kw={'placeholder': 'Comment'})
     submit = SubmitField('Add recommendation')
