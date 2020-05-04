527c656a7646cf845d0bc79d5addb9284a042057
Fabien Roy <fabienroy28@gmail.com>
2020-05-04 18:08:31 -0400

    Add StatService.get... tests

--- tests/admin/fakes.py
+++ tests/admin/fakes.py
@@ -3,18 +3,18 @@ from datetime import datetime
 from app.admin.models import StatEvent
 
 now = datetime.now()
-event1 = StatEvent('USER_LOGIN', now)
-event2 = StatEvent('USER_REGISTER', now)
-event3 = StatEvent('RECOMMENDATION_ADD', now)
+user_login_event = StatEvent('USER_LOGIN', now)
+user_register_event = StatEvent('USER_REGISTER', now)
+recommendation_add_event = StatEvent('RECOMMENDATION_ADD', now)
 
 
 def get_events():
-    return [event1, event2, event3]
+    return [user_login_event, user_register_event, recommendation_add_event]
 
 
 def get_nonnull_stat_event_sums():
-    return {(event1, 0), (event2, 0), (event3, 0)}
+    return {(user_login_event, 0), (user_register_event, 0), (recommendation_add_event, 0)}
 
 
 def get_null_stat_event_sums():
-    return {(event1, 1), (event2, 1), (event3, 1)}
+    return {(user_login_event, 1), (user_register_event, 1), (recommendation_add_event, 1)}
---
+++ tests/admin/test_services.py
@@ -0,0 +1,39 @@
+from app.admin.services import StatService
+from tests.admin.fakes import recommendation_add_event, user_register_event, user_login_event
+from tests.admin.mocks import stat_event_repository
+from tests.interfaces.test_basic import BasicTests
+
+
+class StatServiceTests(BasicTests):
+
+    def setUp(self):
+        self.stat_service = StatService(stat_event_repository)
+
+    def test_get_all_stat_events_sums_with_no_event_should_return_zeros(self):
+        stat_event_repository.get_all.return_value = []
+        event_sums = self.stat_service.get_all_stat_event_sums()
+        self.assert_contains(event_sums, 0, 0, 0)
+
+    def test_get_all_stat_events_sums_with_events_should_return_one_of_each_event(self):
+        for i in range(0, 2):
+            for j in range(0, 2):
+                for k in range(0, 2):
+                    stat_event_repository.get_all.return_value = self.build_events(i, j, k)
+                    event_sums = self.stat_service.get_all_stat_event_sums()
+                    self.assert_contains(event_sums, i, j, k)
+
+    def build_events(self, user_logins, user_registers, recommendation_adds):
+        events = self.build_events_for_amount(user_login_event, user_logins)
+        events.extend(self.build_events_for_amount(user_register_event, user_registers))
+        events.extend(self.build_events_for_amount(recommendation_add_event, recommendation_adds))
+        return events
+
+    @staticmethod
+    def build_events_for_amount(event, amount):
+        return [event] * amount
+
+    def assert_contains(self, event_sums, user_logins, user_registers, recommendation_adds):
+        self.assertEqual(3, len(event_sums))
+        self.assertEqual(event_sums['USER_LOGIN'], user_logins)
+        self.assertEqual(event_sums['USER_REGISTER'], user_registers)
+        self.assertEqual(event_sums['RECOMMENDATION_ADD'], recommendation_adds)
