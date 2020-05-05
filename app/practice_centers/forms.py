d2026e97627fbcf0047f44746b7a7e60f076da7f
Fabien Roy <fabienroy28@gmail.com>
2020-05-05 15:05:52 -0400

    Add search by climate for centers

--- app/practice_centers/forms.py
+++ app/practice_centers/forms.py
@@ -9,3 +9,4 @@ class PracticeCenterSearchForm(GeneralSearchForm):
     email = StringField('Email', validators=[Length(max=100)])
     web_site = StringField('Web site', validators=[Length(max=200)])
     phone_number = StringField('Phone number', validators=[Length(max=20)])
+    climate = StringField('Climate', validators=[Length(max=50)])
