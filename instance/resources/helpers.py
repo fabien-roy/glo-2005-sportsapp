508908aafe389295f07f0786b42d982ae2e6ed7b
Fabien Roy <fabienroy28@gmail.com>
2020-05-05 10:51:40 -0400

    Add importation of users

---
+++ instance/resources/helpers.py
@@ -0,0 +1,20 @@
+import csv
+import pathlib
+
+
+def users_csv():
+    return absolute_path('csv/users.csv')
+
+
+def absolute_path(file):
+    return f'{pathlib.Path(__file__).parent.absolute()}/{file}'
+
+
+def read_elements(file, build_fn):
+    elements = []
+    with open(file, newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        csv_file.readline()
+        for row in reader:
+            elements.append(build_fn(row))
+    return elements
